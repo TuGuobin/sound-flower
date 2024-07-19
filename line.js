e389d0f83111c541c923f1deda891ef242f16ad6
TuGitee <tgb@std.uestc.edu.cn>
2024-07-19 15:33:25 +0800

    animation

--- line.js
+++ line.js
@@ -1,5 +1,5 @@
 class Line {
-    constructor(startX, startY, endX, endY, color = 'black', width = 1, endCircle = false, opacity = 1) {
+    constructor(startX, startY, endX, endY, color = 'black', width = 1, opacity = 1) {
         this.color = color;
         this.width = width;
         this.opacity = opacity;
@@ -14,17 +14,11 @@ class Line {
         ctx.moveTo(this.startX, this.startY);
         ctx.lineTo(this.endX, this.endY);
         ctx.strokeStyle = this.color;
-        ctx.lineWidth = this.width;
+        ctx.lineWidth = this.width * dpr;
         ctx.globalAlpha = this.opacity;
         ctx.lineCap = 'round';
         ctx.lineJoin = 'round';
         ctx.stroke();
-        if (this.endCircle) {
-            ctx.arc(this.endX, this.endY, 2, 0, 2 * Math.PI);
-            ctx.arc(this.startX, this.startY, 2, 0, 2 * Math.PI);
-            ctx.fillStyle = '#0003';
-            ctx.fill();
-        }
         ctx.closePath();
     }
 

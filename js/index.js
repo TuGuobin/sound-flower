883d5eb6545b3d5f03de85060cffb7a387a4e27c
TuGitee <tgb@std.uestc.edu.cn>
2024-07-20 16:03:47 +0800

    confirm message

--- js/index.js
+++ js/index.js
@@ -28,27 +28,63 @@ function showMessage(options = {}) {
         messageList[messageList.length - 1]._removeNode();
         messageList.pop();
     }
-    const { type = 'error', text = '', duration = 3000, isClose = true } = options;
+    const { type = 'error', text = '', duration = 3000, isClose = true, isConfirm = false, isMask = false, confirmText = getNestedValue(language, 'confirm.confirm'), cancelText = getNestedValue(language, 'confirm.cancel'), confirm = () => {}, cancel = () => {} } = options;
     const node = document.createElement('div');
-    node.className = `message ${type}`;
-    node.innerHTML = `<span class="text">${text}</span>`;
+    node.className = 'message-container';
+    document.body.appendChild(node);
+    if (isMask) {
+        const mask = document.createElement('div');
+        mask.className = 'mask';
+        node.appendChild(mask);
+        mask.addEventListener('click', () => {
+            _removeNode();
+        })
+    }
+    const inner = document.createElement('div');
+    inner.className = `message ${type}`;
+    node.appendChild(inner);
+    const textNode = document.createElement('span');
+    textNode.className = 'text';
+    inner.appendChild(textNode);
+    textNode.textContent = text;
     function _removeNode() {
         node.classList.add('hide');
         node.addEventListener('animationend', () => {
             node.remove();
         })
     }
-    const timer = setTimeout(_removeNode, duration);
-    if (isClose) {
+    if (!isConfirm && isClose) {
+        const timer = setTimeout(_removeNode, duration);
         const close = document.createElement('i');
         close.className = 'close';
         close.innerHTML = `<img src="assets/close.svg" alt="">`;
-        node.appendChild(close);
-        document.body.appendChild(node);
+        inner.appendChild(close);
         close.addEventListener('click', () => {
             clearTimeout(timer);
             _removeNode();
         });
+    }
+    if (isConfirm) {
+        inner.classList.add('confirm');
+        const btns = document.createElement('div');
+        btns.className = 'btns';
+        inner.appendChild(btns);
+        const confirm = document.createElement('button');
+        confirm.className = 'success';
+        confirm.innerHTML = confirmText;
+        confirm.addEventListener('click', () => {
+            _removeNode();
+            options.confirm();
+        });
+        btns.appendChild(confirm);
+        const cancel = document.createElement('button');
+        cancel.className = 'cancel';
+        cancel.innerHTML = cancelText;
+        cancel.addEventListener('click', () => {
+            _removeNode();
+            options.cancel();
+        });
+        btns.appendChild(cancel);
     }
     messageList.push({
         node,
